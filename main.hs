757ddad1c70c4d396e4de1a0ccd6b05375889138
Jim <jim.holmstrom@distilnetworks.com>
2017-01-15 23:24:16 +0100

    Adds visual renderings of primitives and fixed bug in intersection

--- main.hs
+++ main.hs
@@ -27,7 +27,7 @@ data Circle = Circle { r :: Radius
 
 infixl 6 .+
 (.+) :: Point -> Point -> Point
-(Point x y) .+ (Point x' y') = Point (x+x') (y-y')
+(Point x y) .+ (Point x' y') = Point (x+x') (y+y')
 
 infixl 6 .-
 (.-) :: Point -> Point -> Point
@@ -45,18 +45,19 @@ determinant :: Line -> Double
 determinant (Line (Point x y) (Point x' y')) = x*y' - x'*y
 
 discriminant :: Circle -> Line -> Double
-discriminant (Circle r) l = r^2 * distance2 l - determinant l
+discriminant (Circle r) l = r^2 * distance2 l - (determinant l)^2
 
-intersection :: Circle -> Line -> Maybe (Point, Point)
+intersection :: Circle -> Line -> Maybe (Point, Point, Point)
 intersection c@(Circle r) l
-    | disc > 0 = Just $ (center .+ diff, center .- diff)
+    | disc > 0 = Just $ (center, center .+ diff, center .- diff)
     | otherwise = Nothing
         where disc = discriminant c l
               det = determinant l
               d2 = distance2 l
               center = Point (det * dy l / d2) (-det * dx l / d2)
-              diff = Point (((signum (dy l) * (dx l) * sqrt disc)) / d2) ((abs (dy l)* sqrt disc) / d2)
-              
+              diff = Point diffX diffY
+              diffX = (signum (dy l) * (dx l) * sqrt disc) / d2
+              diffY = (abs (dy l)* sqrt disc) / d2 
 
 simulationLoop :: SDL.Renderer -> IO ()
 simulationLoop renderer = do
@@ -74,20 +75,46 @@ simulationLoop renderer = do
     SDL.clear renderer
 
     SDL.rendererDrawColor renderer SDL.$= V4 255 255 255 255
-    SDL.drawLine renderer (SDL.P $ V2 0 0) (SDL.P $ V2 64 64)
-    render renderer $ Line (Point 10 24) (Point 100 64)
+--    SDL.drawLine renderer (SDL.P $ V2 0 0) (SDL.P $ V2 64 64)
+
+    SDL.P (V2 x y) <- SDL.getAbsoluteMouseLocation
+    let mouse = Point (fromIntegral x) (fromIntegral y)
+    let circle = Circle 512
+    let line = Line (Point 550 128) (Point 16 512)
+
+    print mouse
+
+    renderCircle renderer circle
+    renderLine renderer line
+
+    case intersection circle line of
+        (Just ps@(c, p, p')) -> do
+            SDL.rendererDrawColor renderer SDL.$= V4 0 255 0 255
+            renderPoint renderer c
+            SDL.rendererDrawColor renderer SDL.$= V4 255 0 0 255
+            renderPoint renderer p
+            renderPoint renderer p'
+        Nothing -> return ()
 
     SDL.present renderer
 
     unless qPressed $ simulationLoop renderer
 
-renderCircle (Circle r) = 
-    where points = map (\t->SDL.P $ V2 (r * cos t) (r * sin t)) $ map (((2*pi)/256)*) [0..255]
+renderPoint renderer p@(Point x y) = do
+    renderLine renderer (Line p (Point (x+8) (y+8)))
+    renderLine renderer (Line p (Point (x+8) (y-8)))
+    renderLine renderer (Line p (Point (x-8) (y+8)))
+    renderLine renderer (Line p (Point (x-8) (y-8)))
+renderLine renderer (Line (Point x y)  (Point x' y')) = SDL.drawLine renderer (SDL.P $ V2 (round x) (round y)) (SDL.P $ V2 (round x') (round y'))
+renderCircle renderer (Circle r) = mapM_ (\(p, p') -> renderLine renderer $ Line p p') $ zip points $ tail points
+    where points = map (\t->Point (r * cos t) (r * sin t)) $ map (((2*pi)/nPoints)*) [0..nPoints]
+          nPoints = 64
 
 main :: IO ()
 main = do
     SDL.initializeAll
     window <- SDL.createWindow "Simulation" SDL.defaultWindow
+
     renderer <- SDL.createRenderer window (-1) SDL.defaultRenderer
 
     simulationLoop renderer
